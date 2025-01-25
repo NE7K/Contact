08b3428b12052c3cee71f59911f33e6c255000c1
NE7K <neighborsoft@gmail.com>
2025-01-25 15:21:25 +0900

    연락처 저장 시스템 추가

--- lib/main.dart
+++ lib/main.dart
@@ -16,36 +16,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
-  var name = ['김영숙', '홍길동', '나구글'];
-  var like = [0, 0, 0];
+
+  var total = 3; // 사람 총 인원
+  var name = ['김영숙', '홍길동', '나구글']; // 사람 이름 저장
+
+  addPerson(a) { // 사람 추가 함수, 매개변수 a
+    setState(() {
+      total++;
+      name.add(a);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         floatingActionButton: FloatingActionButton(
-            child: Text(a.toString()),
+            child: Text('버튼'),
             onPressed: () {
               showDialog(context: context, builder: (context) {
-                return DialogUi(people: a);
+                return DialogUi( addPerson: addPerson);
               });
             }
         ),
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: Text('연락처')
+          title: Text('Contact Appliaction')
         ),
         body: ListView.builder(
-          itemCount: 3,
+          itemCount: name.length, //
           itemBuilder: (context, i) {
             return ListTile(
-              leading: Text(like[i].toString()),
-              title: Text(name[i]),
-              trailing: ElevatedButton(onPressed: () {
-                setState(() {
-                  like[i]++;
-                });
-              }, child: Text('좋아요'))
+              leading: Icon(Icons.account_circle),
+              title: Text(name[i])
             );
           },
         ),
@@ -55,6 +57,47 @@ class _MyAppState extends State<MyApp> {
       );
   }
 }
+
+// floating action button event
+class DialogUi extends StatelessWidget {
+ DialogUi({super.key, this.addPerson});
+
+  final addPerson;
+  var inputData = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+        child: Container(
+          width: 300,
+          height: 200,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('연락처 추가'),
+              TextField( controller: inputData ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {
+                    addPerson(inputData.text);
+                    Navigator.pop(context);
+                    }, child: Text('완료')),
+                  TextButton(onPressed: () {
+                    Navigator.pop(context);
+                    }, child: Text('취소'))
+                ],
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
 
 // 하단 navigator appbar
 class bottomIcon extends StatelessWidget {
@@ -72,40 +115,6 @@ class bottomIcon extends StatelessWidget {
             ]
         )
 
-    );
-  }
-}
-
-// floating action button event
-class DialogUi extends StatelessWidget {
-  DialogUi({super.key, this.people });
-
-  var people;
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-        child: Container(
-          width: 300,
-          height: 200,
-          margin: EdgeInsets.all(10),
-          padding: EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text('Contact'),
-              Text(people.toString()),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(onPressed: () {
-                    Navigator.pop(context);
-                  }, child: Text('cancel'))
-                ],
-              )
-            ],
-          ),
-        )
     );
   }
 }

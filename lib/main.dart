3cbf97dd6147b9d146c52d4a7246039ee98b4c2c
NE7K <sjjang16@naver.com>
2025-01-28 15:46:43 +0900

    Added Contact profile

--- lib/main.dart
+++ lib/main.dart
@@ -19,67 +19,69 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  getPermission() async {
+  getPermission() async { // get permission
     var status = await Permission.contacts.status;
-    if (status.isGranted) {
-      print('허락됨');
-      List<Contact> contacts = await FlutterContacts.getContacts();
+    // await는 다음줄로 넘어가려는 Dart 언어의 특성을 제한하는 기능을 가짐
+    if (status.isGranted) { // permission 허락 받았을 때
+      print('허락됨'); // 출력
+      List<Contact> contacts = await FlutterContacts.getContacts(); // Bring Data = get all Data
 
       setState(() {
-        name = contacts;
+        name = contacts; // 가져온 데이터 name에 저장
       });
 
-    } else if (status.isDenied) {
+    } else if (status.isDenied) { // permission 허락 못 받았을 때
       print('거절됨');
       Permission.contacts.request();
     }
   }
 
   @override
-  void initState() {
-    // TODO: implement initState
+  void initState() { // Appliaction running event
     super.initState();
     getPermission();
-    // openAppSettings(); 앱 설정 띄우기
+    // openAppSettings(); < 앱 설정 띄우기
   }
 
-  var total = 3; // 사람 총 인원
   var name = []; // 사람 이름 저장
+  // List<contacts>를 Dynamic으로 변경해서 타입 오류 방지 but, dynamic = 부하 증가
 
-  addPerson(a) { // 사람 추가 함수, 매개변수 a
-    setState(() {
-      total++;
-      name.add(a);
+  addPerson(contact) { // 사람 추가 함수, 매개변수 a
+    setState( () {
+      name.add(contact);
     });
   }
+
+  var inputData = TextEditingController(); // user text 감시
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         floatingActionButton: FloatingActionButton(
-            child: Text('버튼'),
+            child: Icon(Icons.add),
             onPressed: () {
-              showDialog(context: context, builder: (context) {
-                return DialogUi( addPerson: addPerson);
+              showDialog(context: context, builder: (context) { // Dialog
+                return DialogUi( addPerson: addPerson, inputData: inputData ); // addPerson 함수 DialogUi 위젯으로 사용권한 이전
               });
             }
         ),
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: Text('Contact Appliaction'),
+          title: Text('Contact Application Example'),
           actions: [ IconButton(onPressed: () { getPermission(); }, icon: Icon(Icons.add))],
+          // permission denied = restart action buttton
         ),
         body: ListView.builder(
-          itemCount: name.length, //
+          itemCount: name.length, // 연락처 개수에 따라 변동
           itemBuilder: (context, i) {
             return ListTile(
               leading: Icon(Icons.account_circle),
-              title: Text(name[i].displayName)
+              title: Text(name[i].displayName) // 연락처의 displayname 부분만 카피
             );
           },
         ),
 
-        bottomNavigationBar: bottomIcon()
+        bottomNavigationBar: bottomIcon() // bottom widget
 
       );
   }
@@ -87,10 +89,10 @@ class _MyAppState extends State<MyApp> {
 
 // floating action button event
 class DialogUi extends StatelessWidget {
- DialogUi({super.key, this.addPerson});
+ DialogUi({super.key, this.addPerson, this.inputData});
 
-  final addPerson;
-  var inputData = TextEditingController();
+ final addPerson;
+ final inputData;
 
   @override
   Widget build(BuildContext context) {
@@ -103,13 +105,20 @@ class DialogUi extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text('연락처 추가'),
+              Text('이름을 입력해주세요'),
               TextField( controller: inputData ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(onPressed: () {
-                    addPerson(inputData.text);
+
+                    // Insert new contact
+                    final newContact = Contact();
+                    newContact.name.first = inputData.text;
+                    newContact.displayName = inputData.text;
+                    newContact.insert();
+                    addPerson(newContact);
+
                     Navigator.pop(context);
                     }, child: Text('완료')),
                   TextButton(onPressed: () {
